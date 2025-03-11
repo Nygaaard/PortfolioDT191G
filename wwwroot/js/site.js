2ec10b0bda98ee86fbfeeb84a8130f0524df718d
Nygaaard <nygardandreas@hotmail.com>
2025-03-11 15:13:38 +0100

    Added dark/light mode toggle and css

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,9 +1,15 @@
-﻿const openIcon = document.querySelector('.menu-icon');
+﻿//Variables
+//Hamburger
+const openIcon = document.querySelector('.menu-icon');
 const menu = document.querySelector('.menu');
-
 const closeIcon = document.querySelector('.menu-close-icon');
 
+//Dark/light mode
+const lightModeToggle = document.querySelector('.light-container'); 
+const body = document.body; 
+const icon = document.querySelector('.dark-light');
 
+//Hamburger toggle
 openIcon.addEventListener('click', function () {
     menu.classList.toggle('show'); 
     closeIcon.classList.toggle('show')
@@ -12,5 +18,18 @@ openIcon.addEventListener('click', function () {
 closeIcon.addEventListener('click', function () {
     menu.classList.toggle('show'); 
     closeIcon.classList.toggle('show')
+});
+
+//Dark/light toggle
+lightModeToggle.addEventListener('click', function () {
+    body.classList.toggle('light-mode');
+    
+    if (body.classList.contains('light-mode')) {
+        icon.classList.remove('fa-circle-half-stroke');
+        icon.classList.add('fa-sun');
+    } else {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-circle-half-stroke');
+    }
 });
 
