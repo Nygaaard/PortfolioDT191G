776f2bbb0e0164644ce1767b25df214368b27430
Nygaaard <nygardandreas@hotmail.com>
2025-03-11 12:16:28 +0100

    Added context to HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using PortfolioDT191G.Data;
 using PortfolioDT191G.Models;
 
 namespace PortfolioDT191G.Controllers;
@@ -7,15 +8,30 @@ namespace PortfolioDT191G.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    //Added context
+    private readonly ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
     {
-        return View();
+        //Hämta data från databasen
+        var courses = _context.Courses.ToList(); //Hämta Courses
+        var skills = _context.Skills.ToList(); // Hämta Skills
+        var frameworks = _context.Frameworks.ToList(); // Hämta Ramverk
+
+        //Skapa ViewModel
+        var viewModel = new HomeModel
+        {
+            Courses = courses,
+            Skills = skills,
+            Frameworks = frameworks
+        };
+        return View(viewModel);
     }
 
     public IActionResult Privacy()
