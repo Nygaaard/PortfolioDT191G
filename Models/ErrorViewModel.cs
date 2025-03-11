095756e3b2449f477dc37b3ff6f8c6a5bb26d4b9
Nygaaard <nygardandreas@hotmail.com>
2025-03-11 10:20:17 +0100

    Initialized project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PortfolioDT191G.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
